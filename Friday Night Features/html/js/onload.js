1c2a47ebb90a5090c1dbd2de46cd6d03c6777723
nolietrex <noelguillory@gmail.com>
2015-06-15 15:30:26 -0600

    FNF: the custom js I'm writing to get this component going

---
+++ Friday Night Features/html/js/onload.js
@@ -0,0 +1,173 @@
+$(document).ready(function(){
+
+	// =LISTS
+	$(function() {
+		// Add classes to first and last of each list
+		$('li:first-child').addClass('js-first');
+		$('li:last-child').addClass('js-last');
+	});
+	
+	var currentDate = new Date(); //get current date
+	var n = 376; //.width of number AFTER animation won't measure so this is 350 + 10 margin + 3 border
+	var selectedMovie;
+	var pageWidth;
+	var itemNumber;
+	var itemWidth;
+	var fullWidth;
+	var numBeforeCurrent;
+	
+	$(window).resize(windowResize);
+	
+	$('.carousel-item').click(function(){
+		selectMovie($(this));
+	});
+	
+	
+	function loadCarouselImages(){
+		$('.carousel-item').each(function(){
+			var bg = $(this).attr('headerPhoto');
+			$(this).css('background-image','url(images/'+bg+')');
+		});
+	}
+	
+	function detirmineInitialMovie(){
+		$('.carousel-item').each(function(){
+			
+			var itemDate = new Date($(this).attr('date'));
+			
+			if (currentDate < itemDate){
+				var t = $(this);
+				var id = t.attr('id')
+				
+				selectMovie(t);
+				return false;
+			}
+		});
+	}
+	
+	function selectMovie(t){
+		var id = t.attr('id')
+
+		$('#'+selectedMovie).removeClass('js-currentMovie');
+		$('#'+id).addClass('js-currentMovie');
+		
+		selectedMovie = id;
+		countToCurrent();
+		animateOver();
+		selectDetail();
+	}
+
+	function selectDetail(){
+		var theId = $('.js-currentMovie').attr('id')
+		if($('.carousel-item').hasClass('js-currentMovie')){
+			$('.detail').removeClass('js-currentDetail');
+			$('.detail' + '#' + theId).addClass('js-currentDetail');
+			console.log(theId, 'is the carousel-item id attr');
+		} else {
+			$('.detail' + '#' + theId).removeClass('js-currentDetail');
+		}
+		// $('.detail').each(function(){
+
+		// 	var id = $(this).attr('id')
+		// 	$('#'+selectedMovie).removeClass('js-currentMovie');
+		// 	$('#'+id).addClass('js-currentMovie');
+		// 	selectedMovie = id;
+		// 	// if($('#'+id).hasClass('js-currentMovie')) {
+		// 	// 	$(this).addClass('js-currentDetail');
+		// 	// } else {
+		// 	// 	$(this).removeClass('js-currentDetail');
+		// 	// }
+		// });
+	}
+	selectDetail();
+
+	// =ARROWS
+	$('.js-arrow-right').click($.throttle(function(){
+		// if this is the last item, go back to first item
+		if($('.carousel-item.js-currentMovie').hasClass('js-last')) {
+			selectMovie($('.carousel-item').first());
+		}
+		// else highlight next
+		else {
+			selectMovie($('.carousel-item.js-currentMovie').next());
+		}
+	}, 500));
+
+	$('.js-arrow-left').click($.throttle(function(){
+		// if this is the first item, go to the last item
+		if($('.carousel-item.js-currentMovie').hasClass('js-first')) {
+			selectMovie($('.carousel-item').last());
+		}
+		// else highlight previous
+		else {
+			selectMovie($('.carousel-item.js-currentMovie').prev());
+		}
+	}, 500));
+
+
+	function windowResize(){
+		//measure the page width
+		pageWidth = $(window).width();
+		// console.log(pageWidth);
+		moveOver();
+	}
+
+
+	function countItems() {
+		//count the carousel items
+		itemNumber = $('.carousel-item').length;
+		// console.log(itemNumber);
+	}
+
+
+	function countToCurrent(){
+		//counts how many are before the selected div
+		numBeforeCurrent = $('.js-currentMovie').prevAll().length
+		// console.log("there are", numBeforeCurrent, "before this one");
+	}
+
+
+	$('.carousel-item').each(function() {
+		//measure the width of each item
+		itemWidth = $(this).outerWidth(true);
+		// console.log(itemWidth);
+	});
+
+
+	function calcfullWidth() {
+		fullWidth = 0;
+		// add the sum of all of the widths of each carousel item
+		$('.carousel-item').each(function(index) {
+			fullWidth += parseInt($(this).outerWidth(true),10)
+		});
+
+		//set width of container to sum of all carousel items
+		$('ul#carousel-items').css({'width' : fullWidth + '2em'}); //couldn't get it to measure AFTER animation so added 2em hack
+
+		// console.log("full width", fullWidth);
+	}
+
+	function moveOver() {
+		var thisFar = (numBeforeCurrent * itemWidth) - (pageWidth * 0.5) + (n / 2); 
+		$('.carousel-item').css({'left' : -thisFar + 'px'});
+		// console.log("move over this far", thisFar);
+		// console.log("n is this", n);
+	}
+
+	function animateOver() {
+		var thisFar = (numBeforeCurrent * itemWidth) - (pageWidth * 0.5) + (n / 2); 
+		$('.carousel-item').animate({'left' : -thisFar + 'px'});
+		// console.log("move over this far", thisFar);
+		// console.log("n is this", n);
+	}
+
+
+	loadCarouselImages();
+	detirmineInitialMovie();
+	windowResize();
+	countItems();
+	calcfullWidth();
+	countToCurrent();
+	moveOver();
+	
+});
