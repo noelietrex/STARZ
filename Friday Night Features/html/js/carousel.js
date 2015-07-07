1a286575a23c824be7f5d2dbb2992e40cfabbf19
nolietrex <noelguillory@gmail.com>
2015-07-07 14:50:22 -0600

    trying to unfuck take 2

--- Friday Night Features/html/js/carousel.js
+++ Friday Night Features/html/js/carousel.js
@@ -8,17 +8,13 @@
         	children: '.children-item',
         	onLoaded: function(){},
 			onClick: function(){},
+			activeWidth: 376
     	}, options || {}) 
     	var globalVariableObject = {}
     	
     	this.el = $el;
     	this.init = function(){
 			loadjson();
-	    	//LOAD JSON
-	    	
-	    	//ADJUST JSON TO ADD DATE FIELDS
-	    	//LOAD TEMPLATE
-	    	//ADD CLICK EVENT
     	}
 
     	function loadjson(){
@@ -49,32 +45,38 @@
 		function onjsonLoaded(){
 			addClickEvent();
 			loadFirstMovie();
+			measure();
+			center();
 			arrows();
+			windowResize();
+			$(window).resize(windowResize);
 		}
 
 		function arrows() {
-			var last = $el.find(settings.children).length
-		
-			console.log('how many:', last); 
+			var length = $el.find(settings.children).length
+			var last = $('#' + 'carousel' + (length - 1))
+			
 			$('.js-arrow-right').click($.throttle(function(){
-				console.log('right arrow clicked');
-				// if (){
+				if ($(last).hasClass('js-currentMovie')){
+					$el.find('#carousel0').trigger("click");
+				} else {
+					$el.find('.js-currentMovie').next().trigger("click");
+				}
+			}, 500));
 
-				// } else {
-				// 	$el.find('.js-currentMovie').next().trigger("click");
-				// }
+			$('.js-arrow-left').click($.throttle(function(){
+				if ($('#carousel0').hasClass('js-currentMovie')){
+					$el.find(last).trigger("click");
+				} else {
+					$el.find('.js-currentMovie').prev().trigger("click");
+				}
 			}, 500));
 		}
 
-		// function arrows(){
-		// 	$('.js-arrow-right').click($.throttle(function(){
-		// 		console.log('right arrow clicked');
-		// 		$el.find(settings.children + 'js-currentMovie').next(selectMovie);
-		// 	}, 500));
-		// }
-
 		function addClickEvent(){
-			$el.find(settings.children).click(selectMovie)
+			if ($(window).width() >= 750){	
+				$el.find(settings.children).click(selectMovie)
+			}
 		}
 
 		function loadFirstMovie(){
@@ -90,15 +92,66 @@
 			});
 		}
 
-
-
 		function selectMovie(){
 			var t = $(this)
 
-			settings.onClick.call(this, t.data('moviedata'));
+			if ($(window).width() >= 750){	
+				settings.onClick.call(this, t.data('moviedata'));
 
-			$el.find(settings.children).removeClass('js-currentMovie');
-			$(t).addClass('js-currentMovie');
+				$el.find(settings.children).removeClass('js-currentMovie');
+				$(t).addClass('js-currentMovie');
+
+				move();
+			}
+		}
+
+		function measure(){
+			var x = $el.find(settings.children)
+			var carItemWidth = x.outerWidth(true);
+			// var activeWidth = $el.find(settings.children + '.js-currentMovie').outerWidth(true);
+			var countToCurrent = $el.find(settings.children + '.js-currentMovie').prevAll().length;
+			var carouselNum = x.length;
+			var carouselWidth = ((carouselNum - 1) * carItemWidth) + settings.activeWidth
+
+			console.log('carItemWidth: ', carItemWidth);
+			console.log('activeWidth: ', settings.activeWidth);
+			console.log('countToCurrent: ', countToCurrent);
+			console.log('carouselWidth:', carouselWidth);
+
+			$el.css({'width' : carouselWidth + 'px'});
+		}
+
+		var activeWidth;
+
+		function center(){
+			var x = $el.find(settings.children)
+			var carItemWidth = x.outerWidth(true);
+			// var activeWidth = $el.find(settings.children + '.js-currentMovie').outerWidth(true); // HOW DO I MAKE THIS THE NUMBER FROM AFTER LOAD?
+			var countToCurrent = $el.find(settings.children + '.js-currentMovie').prevAll().length;
+			var carouselNum = x.length;
+			var carouselWidth = ((carouselNum - 1) * carItemWidth) + settings.activeWidth
+			var screenWidth = $(window).width()
+			var howFar = (countToCurrent * carItemWidth) - (screenWidth / 2) + (settings.activeWidth / 2)
+
+			$el.css({'left': -howFar + 'px'});
+		}
+
+		function move(){
+			var x = $el.find(settings.children)
+			var carItemWidth = x.outerWidth(true);
+			// var activeWidth = $el.find(settings.children + '.js-currentMovie').outerWidth(true); // HOW DO I MAKE THIS THE NUMBER FROM AFTER LOAD?
+			var countToCurrent = $el.find(settings.children + '.js-currentMovie').prevAll().length;
+			var carouselNum = x.length;
+			var carouselWidth = ((carouselNum - 1) * carItemWidth) + settings.activeWidth
+			var screenWidth = $(window).width()
+			var howFar = (countToCurrent * carItemWidth) - (screenWidth / 2) + (settings.activeWidth / 2)
+
+			$el.animate({'left': -howFar + 'px'});
+		}
+
+		function windowResize(){
+			pageWidth = $(window).width();
+			center();
 		}
 
 
@@ -116,4 +169,3 @@
 	}
     
 })(jQuery, window, document);
-
